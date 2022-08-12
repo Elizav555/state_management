5da6b40c699433954ed2eb36051d3899ed267c7d
Elizav555 <lizagarkina5@gmail.com>
2022-08-12 13:16:57 +0400

    in progress

--- lib/ui/my_app.dart
+++ lib/ui/my_app.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_architecture/notifiers/cart_notifier.dart';
-import 'package:provider/provider.dart';
 
 import 'shop_list.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<CartState>(
-        create: (_) => CartState(),
-        child: MaterialApp(
-          title: 'Shop',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: ShopListWidget(),
-        ));
+    return MaterialApp(
+      title: 'Shop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ShopListWidget(null),
+    );
   }
 }
--- lib/ui/shop_list.dart
+++ lib/ui/shop_list.dart
@@ -1,72 +1,99 @@
 import 'package:flutter/material.dart';
-import 'package:provider/src/provider.dart';
+import 'package:flutter_architecture/bloc/cart_bloc.dart';
+import 'package:flutter_architecture/model/shopping_cart.dart';
 
 import '../model/item.dart';
-import '../notifiers/cart_notifier.dart';
 import 'cart_list.dart';
 
-class ShopListWidget extends StatelessWidget {
+class ShopListWidget extends StatefulWidget {
+  const ShopListWidget(Key? key) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => ShopListState();
+}
+
+class ShopListState extends State<ShopListWidget> {
+  late final CartBloc bloc;
+
+  @override
+  void initState() {
+    super.initState();
+    bloc = CartBloc();
+  }
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final shop_items = Item.dummyItems;
-    final cart = context.watch<CartState>().cart;
-    final columnCount =
-        MediaQuery.of(context).orientation == Orientation.portrait ? 2 : 4;
+    return StreamBuilder<ShoppingCart>(
+        stream: bloc.cartState,
+        builder: (context, snapshot) {
+          final shop_items = Item.dummyItems;
+          final cart = snapshot.data;
+          final columnCount =
+              MediaQuery.of(context).orientation == Orientation.portrait
+                  ? 2
+                  : 4;
 
-    final width = MediaQuery.of(context).size.width / columnCount;
-    const height = 400;
+          final width = MediaQuery.of(context).size.width / columnCount;
+          const height = 400;
 
-    var items = <Widget>[];
-    for (var x = 0; x < shop_items.length; x++) {
-      bool isSideLine;
-      if (columnCount == 2) {
-        isSideLine = x % 2 == 0;
-      } else {
-        isSideLine = x % 4 != 3;
-      }
-      final item = shop_items[x];
+          var items = <Widget>[];
+          for (var x = 0; x < shop_items.length; x++) {
+            bool isSideLine;
+            if (columnCount == 2) {
+              isSideLine = x % 2 == 0;
+            } else {
+              isSideLine = x % 4 != 3;
+            }
+            final item = shop_items[x];
 
-      items.add(_ShopListItem(
-        item: item,
-        isInCart: cart.isExists(item),
-        isSideLine: isSideLine,
-        onTap: (item) {
-          ScaffoldMessenger.of(context).hideCurrentSnackBar();
-          if (cart.isExists(item)) {
-            context.read<CartState>().removeFromCart(item);
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text('Item is removed from cart!'),
-            ));
-          } else {
-            context.read<CartState>().addToCart(item);
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text('Item is added to cart!'),
+            items.add(_ShopListItem(
+              item: item,
+              isInCart: cart?.isExists(item) ?? false,
+              isSideLine: isSideLine,
+              onTap: (item) {
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                if (cart?.isExists(item) == true) {
+                  bloc.action.add(RemoveFromCartEvent(item: item));
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text('Item is removed from cart!'),
+                  ));
+                } else {
+                  bloc.action.add(AddToCartEvent(item: item));
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text('Item is added to cart!'),
+                  ));
+                }
+              },
             ));
           }
-        },
-      ));
-    }
 
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Store'),
-        ),
-        body: GridView.count(
-          childAspectRatio: width / height,
-          scrollDirection: Axis.vertical,
-          crossAxisCount: columnCount,
-          children: items,
-        ),
-        floatingActionButton: cart.isEmpty
-            ? null
-            : FloatingActionButton.extended(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => CartListWidget()));
-                },
-                icon: Icon(Icons.shopping_cart),
-                label: Text('${cart.numOfItems}'),
-              ));
+          return Scaffold(
+              appBar: AppBar(
+                title: Text('Store'),
+              ),
+              body: GridView.count(
+                childAspectRatio: width / height,
+                scrollDirection: Axis.vertical,
+                crossAxisCount: columnCount,
+                children: items,
+              ),
+              floatingActionButton: cart?.isEmpty == false
+                  ? FloatingActionButton.extended(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => CartListWidget(null)));
+                      },
+                      icon: Icon(Icons.shopping_cart),
+                      label: Text('${cart?.numOfItems}'),
+                    )
+                  : null);
+        });
   }
 }
 
